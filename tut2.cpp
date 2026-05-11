c29cc2d8d140e1bc0f4cc956a076771f38b929cc
Ayush Kumar <rk5535710@gmail.com>
2026-05-11 20:50:20 +0530

    Initial C++ Hello World programs

---
+++ tut2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout << "The sum of a and b is: " << c << endl;
+    return 0;
+}
