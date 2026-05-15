9a0e5955f8b60075be84d0076241e9c4c772044d
Ayush Kumar <rk5535710@gmail.com>
2026-05-15 22:38:44 +0530

    Day 2: Local vs Global Variables.

---
+++ tut3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int glo=6;
+void sum(){
+    int a; 
+    cout<<glo;
+}
+
+int main(){
+    int glo=9;
+    glo=78;
+    // int a=4;
+    // int b=5;
+    int a=14,b=15;
+    float pi=3.14;
+    char c='A';
+    sum();
+    cout<<glo;
+//     cout<<"This is tutorial 3.\nHere, The valuse of a is "<<a<<".\nThe value of b is "<< b;
+
+// cout<<"\nThe value of pi is "<<3.14;
+// cout<<"\nThe value of c is "<<c;
+    return 0;
+
+
+}
